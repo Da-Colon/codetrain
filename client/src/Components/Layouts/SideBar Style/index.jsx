bb6e995f4b0005d3ebec647ecd3c1014b6a64f45
Nep T. Orshiso <nep.orshiso@gmail.com>
2019-12-10 14:18:33 -0500

    experimenting with styled components

---
+++ client/src/Components/Layouts/SideBar Style/index.jsx
@@ -0,0 +1,14 @@
+import styled, { css } from "styled-components";
+
+const SideBar = styled.button`
+    height: 100%;
+    width: 100px;
+    position: fixed; 
+    z-index: 1;
+    top: 0; 
+    left: 0;
+    background-color: #111;
+    overflow-x: hidden; 
+`;
+
+export default SideBar;
---
+++ client/src/Components/SideBar/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import SideBar from '../Layouts/SideBar Style'
+
+function index() {
+    return (
+        <div>
+            <SideBar>
+                <p>This is content</p>
+            </SideBar>
+        </div>
+    )
+}
+
+export default index
