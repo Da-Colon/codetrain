e37b9ba381cede20c99c17b3f5c7bc94f967e4d7
Da-Colon <dcolonantonio@gmail.com>
2019-12-10 11:15:48 -0500

    made changs to schema; combined with prescotts prototype

--- client/src/SQL/schema.sql
+++ client/src/SQL/schema.sql
@@ -1,31 +1,38 @@
+CREATE TABLE user_types (
+  id SERIAL PRIMARY KEY,
+  type TEXT
+);
+
+INSERT INTO user_types (
+  id, type
+) VALUES 
+  (1, 'administrator'), 
+  (2, 'bootcamper'), 
+  (3, 'company');
+
+CREATE TABLE companies (
+  id SERIAL PRIMARY KEY,
+  email VARCHAR(50) NOT NULL,
+  name VARCHAR(100),
+  company_url VARCHAR(125),
+  company_logo_url VARCHAR(125),
+  description TEXT
+);
+
 CREATE TABLE users (
   id SERIAL PRIMARY KEY,
   email VARCHAR(25) NOT NULL,
   password VARCHAR(12) NOT NULL,
   first_name VARCHAR(25) NOT NULL,
   last_name VARCHAR(25) NOT NULL,
+  skills TEXT[],
   github_url VARCHAR(125) NOT NULL,
   linkedin_url VARCHAR(125) NOT NULL,
   auth BOOLEAN,
-  bootcamp_name VARCHAR(100)
+  user_types_id INTEGER REFERENCES user_types(id),
+  bootcamp_name VARCHAR(100),
+  companies_id INTEGER REFERENCES companies(id)
 );
-
-CREATE TABLE user_profile (
-  skills TEXT[],
-  user_id INTEGER REFERENCES users(id)
-);
-
-
-CREATE TABLE company (
-  id SERIAL PRIMARY KEY,
-  email VARCHAR(25) NOT NULL,
-  company_url VARCHAR(125),
-  company_logo_url VARCHAR(125),
-);
-
-CREATE TABLE company_users (
-  company_id INTEGER REFERENCES company(id),
-) INHERITS (users);
 
 CREATE TABLE posts_resources (
   id SERIAL PRIMARY KEY,
@@ -34,26 +41,44 @@ CREATE TABLE posts_resources (
   title VARCHAR(100),
   short_description VARCHAR(400),
   full_description text,
-  resource_url VARCHAR(125)
+  resource_url VARCHAR(125),
+  date_posted DATE DEFAULT NOW()
 );
 
-CREATE TABLE post_job_board (
+CREATE TABLE posts_jobs (
   id SERIAL PRIMARY KEY,
-  title VARCHAR(100),
-  content TEXT,
+  title VARCHAR(100) NOT NULL,
+  content TEXT NOT NULL,
   experience TEXT,
-  contact_email VARCHAR(25),
-  contact_phone VARCHAR(10),
-  company_id INTEGER REFERENCES company(id),
-  company_users_id INTEGER REFERENCES company_users(id)
+  date_posted DATE DEFAULT NOW(),
+  contact_email VARCHAR(50),
+  contact_phone VARCHAR(15),
+  companies_id INTEGER REFERENCES companies(id),
+  users_id INTEGER REFERENCES users(id)
+);
+
+CREATE TABLE job_applications (
+  id SERIAL PRIMARY KEY,
+  date_applied DATE DEFAULT NOW(),
+  rejected BOOLEAN,
+  accepted BOOLEAN,
+  users_id INTEGER REFERENCES users(id),
+  posts_jobs_id INTEGER REFERENCES posts_jobs(id)
 );
 
 CREATE TABLE private_messages(
   id SERIAL PRIMARY KEY,
-  user_id INTEGER REFERENCES users(id),
-  company_id INTEGER REFERENCES company(id),
-  company_users_id INTEGER REFERENCES company_users(id)
+  subject TEXT,
+  message TEXT,
+  sent_from INTEGER REFERENCES users(id),
+  sent_to INTEGER REFERENCES users(id),
+  date_sent DATE DEFAULT NOW()
 );
+
+CREATE TABLE preferences(
+  id SERIAL PRIMARY KEY,
+  users_id INTEGER REFERENCES users(id)
+)
 
 -- Need to Figure out Data Storage for PDFs
 
