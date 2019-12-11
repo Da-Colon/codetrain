6ce342ba879681cd6d984b102bf5fddd439ecea8
Da-Colon <dcolonantonio@gmail.com>
2019-12-11 14:44:17 -0500

    added POST route for resources to save new resource to database

--- server/models/resourcePostModel.js
+++ server/models/resourcePostModel.js
@@ -1,16 +1,30 @@
-const db = require('./conn');
+const db = require("./conn");
 
 const getAllResources = async () => {
-    try{
-      return await db.any(`SELECT * FROM posts_resources ORDER BY date_posted DESC LIMIT 5;`)
-    }catch{
-      console.log(`Error: Unable to retrieve resources from database`)
-    }
+  const query = await db.any(
+    `SELECT * FROM posts_resources ORDER BY date_posted DESC LIMIT 5;`
+  );
+  try {
+    return query;
+  } catch {
+    return console.log(`Error: Unable to retrieve resources from database`);
   }
+};
 
+const saveNewResource = async (
+  title,
+  short_description,
+  full_description,
+  resource_url,
+  userId
+) => {
+  const query = await db.any(`INSERT INTO posts_resources (title, short_description, full_description, resource_url, users_id) VALUES ($1, $2, $3, $4, $5);`, [title, short_description, full_description, resource_url, userId]
+  );
+  try {
+    return query;
+  } catch {
+    return console.log(`ERROR: Unable to save to database`);
+  }
+};
 
-// const saveNewResource = () => {
-// }
-
-
-module.exports = {getAllResources}
+module.exports = { getAllResources, saveNewResource };
--- server/routes/resourcePosts.js
+++ server/routes/resourcePosts.js
@@ -8,8 +8,11 @@ router.get('/getAllResources', async (res, req, next) => {
   req.send(getResources).status(200).end();
 })
 
-// router.post('saveNewResource', (req, res, next) => {
-
-// })
+router.post("/add/:user_id", async (req, res) => {
+  const userId = req.params.user_id;
+  const {title, short_description, full_description, resource_url} = req.body;
+  await ResourcePostModel.saveNewResource(title, short_description, full_description, resource_url, userId );
+  res.json({ message: 'Successfully Saved' }).status(200);
+});
 
 module.exports = router;
