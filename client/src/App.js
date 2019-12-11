dc4eed2d9f033e01933d5216af7d30ab62e3bbce
Daniel Farlow <dan.farlow@gmail.com>
2019-12-11 01:52:38 -0500

    add most basic of router functionality for navbar and sidebar

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,29 @@
 import React from 'react';
 import NavBar from './components/NavBar/NavBar';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import Sidenav from './components/Sidenav/Sidenav'
-
-
-import './App.css';
+import Sidebar from './components/Sidebar/Sidebar';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 const Login = () => <h1>Login</h1>
-const Signup = () => <h1>Sign in</h1>
+const Signup = () => <h1>Sign up</h1>
+
+const Dashboard = () => <h1>This is the Dashboard</h1>
+const Profile = () => <h1>This is the Profile</h1>
+const Jobs = () => <h1>This is the Jobs</h1>
+const Resources = () => <h1>This is the Resources</h1>
 
 function App() {
   return (
     <Router>
-
       <Route path="/" component={NavBar} />
+      <Route path="/" component={Sidebar} />
       <Route exact path="/login" component={Login} />
       <Route exact path="/signup" component={Signup} />
-      <Route exact path="/sidebar" component={Sidenav} />
+      <Route exact path="/dashboard" component={Dashboard} />
+      <Route exact path="/profile" component={Profile} />
+      <Route exact path="/jobs" component={Jobs} />
+      <Route exact path="/resources" component={Resources} />
     </Router>
   );
 }
 
-export default App;
+export default App
--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,18 +1,15 @@
 import React from 'react';
 
-import { BrowserRouter as Router, Route, Link, NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const NavBar = (props) => {
-  console.log(props);
   return (
-    <nav className="black">
-      <div className="nav-wrapper">
+    <nav>
         <NavLink exact to="/" className="brand-logo">codetrain</NavLink>
-        <ul id="nav-mobile" className="right hide-on-med-and-down">
+        <ul>
           <li><NavLink to="/signup">Sign up</NavLink></li>
           <li><NavLink to="/login">Log in</NavLink></li>
         </ul>
-      </div>
     </nav>
   );
 }
