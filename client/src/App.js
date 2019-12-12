291f192e84f8a663cf702f91788b7ff487d94739
Da-Colon <dcolonantonio@gmail.com>
2019-12-11 23:40:31 -0500

    moved components into new component folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,49 +1,25 @@
 import React from 'react';
-import BootcampApplications from './components/BootcampApplications/BootcampApplications';
-import CompanyApplications from './components/CompanyApplications/CompanyApplications';
-import BootcampProfile from './components/BootcampProfile/BootcampProfile';
-import CompanyProfile from './components/CompanyProfile/CompanyProfile';
-import Login from './components/Login/Login';
-import Signup from './components/Signup/Signup';
-import NavBar from './components/NavBar/NavBar';
-import BootcampJobs from './components/BootcampJobs/BootcampJobs';
-import CompanyJobs from './components/CompanyJobs/CompanyJobs';
-import Resources from './components/Resources/Resources';
-import Sidebar from './components/Sidebar/Sidebar';
-import FrozenDept from './components/FrozenDept/FrozenDept';
-import BootcampDashboard from './components/BootcampDashboard/BootcampDashboard';
-import CompanyDashboard from './components/CompanyDashboard/CompanyDashboard';
-import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+
+import { BrowserRouter as Router, Route} from "react-router-dom";
+
+import NavBar from './component/NavBar/NavBar';
+import Signup from './component/Signup/Signup';
+import Login from './component/Login/Login'
 
 import "./App.css";
 
-// const userType = 'bootcamper'
-const userType = 'company'
-function App() {
+
+
+const App = () => {
   return (
+    <>
+      <NavBar />
     <Router>
-    {/* Shared Routes  */}
-      <Route path="/" component={NavBar} />
-      <Route path="/" component={Sidebar} />
-      <Route exact path="/login" component={Login} />
-      <Route exact path="/signup" component={Signup} />
-      <Route exact path="/applications" render={() => {
-        return userType === 'bootcamper' ? <BootcampApplications /> : <CompanyApplications />
-      }} />
-      <Route exact path="/dashboard" render={() => {
-        return userType === 'bootcamper' ? <BootcampDashboard /> : <CompanyDashboard />
-      }} />
-      <Route exact path="/jobs" render={() => {
-        return userType === 'bootcamper' ? <BootcampJobs /> : <CompanyJobs />
-      }} />
-      <Route exact path="/profile" render={() => {
-        return userType === 'bootcamper' ? <BootcampProfile /> : <CompanyProfile />
-      }} />
-      {/* Bootcamper Routes */}
-      <Route exact path="/resources" component={Resources} />
-      {/* Reference Routes */}
-      <Route exact path="/frozen-dept" component={FrozenDept} />
+      <Route path="/" component={Home} />
+      <Route path="/signup" component={Signup} />
+      <Route path="/login" component={Login} />
     </Router>
+    </>
   );
 }
 
