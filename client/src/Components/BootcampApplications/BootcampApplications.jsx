fffef838a85081eb27be28d900adb11c6bb25027
Daniel Farlow <dan.farlow@gmail.com>
2019-12-11 22:13:38 -0500

    create ability for bootcamper to apply for jobs

--- client/src/Components/BootcampApplications/BootcampApplications.jsx
+++ client/src/Components/BootcampApplications/BootcampApplications.jsx
@@ -1,12 +1,58 @@
-import React, { Component} from 'react';
+import React, { useState, Fragment} from 'react';
+import axios from "axios";
 
-class BootcampApplications extends Component {
+const BootcampApplications = () => {
+  const [state, setState] = useState({
+    date_applied: new Date(),
+    rejected: false,
+    accepted: false,
+    applicationSubmitted: false
+  });
 
-  render() { 
-    return ( 
-      <h1>This is the BootcampAPPLICATIONS component</h1>
-    );
-  }
-}
- 
+  const handleChange = e => {
+    const { name, value } = e.target;
+    setState({ ...state, [name]: Number(value) });
+  };
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    const endpoint = "http://localhost:3000/job-applications/add-application";
+
+    const payload = {
+      date_applied: state.date_applied,
+      rejected: state.rejected,
+      accepted: state.accepted,
+      users_id: 1, /* we need to pull this from session or something similar */
+      posts_jobs_id: state.posts_jobs_id
+    };
+
+    const response = await axios.post(endpoint, payload);
+    setState({ ...state, applicationSubmitted: true });
+  };
+
+  return (
+    <Fragment>
+      <h1>Submit an Application</h1>
+      {state.applicationSubmitted ? (
+        <div>
+          <h2>Thank you for submitting an application! We'll be in touch shortly.</h2>
+        </div>
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <label>
+            Job to which you are applying:
+            <input
+              type="number"
+              name="posts_jobs_id"
+              value={state.name}
+              onChange={handleChange}
+            ></input>
+          </label>
+          <button type="submit">Submit Application</button>
+        </form>
+      )}
+    </Fragment>
+  );
+};
+
 export default BootcampApplications;
