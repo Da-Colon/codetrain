3d64c94c82f6067ec6156797d0ad716df0a62b06
Da-Colon <dcolonantonio@gmail.com>
2019-12-13 00:22:17 -0500

    added development variables to change user type and auth

--- client/src/reducers/loginReducer.js
+++ client/src/reducers/loginReducer.js
@@ -1,3 +1,5 @@
+const developmentType = 2
+const developmentAuth = true
 const initialState = {
     id: null,
     email: null,
@@ -5,8 +7,8 @@ const initialState = {
     last_name: null,
     skills: null,
     linkedin_url: null,
-    auth: null,
-    user_types_id: null,
+    auth: developmentAuth,
+    user_types_id: developmentType,
     bootcamp_name: null,
     companies_id: null
 };
