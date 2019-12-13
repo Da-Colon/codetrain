10623a2fc40f8ceb5fb0e8f8908bf86f6fa05254
Prescott Sun <sunprescott@gmail.com>
2019-12-12 22:58:02 -0500

    adds company profile view component

--- client/src/component/Dashboard/Dashboards/Bootcamp/BootcampDashboard.jsx
+++ client/src/component/Dashboard/Dashboards/Bootcamp/BootcampDashboard.jsx
@@ -2,11 +2,13 @@ import React, { Component, Fragment } from 'react';
 import HackerNews from '../../../NewsFeed/HackerNews/'
 import TechNews from '../../../NewsFeed/TechNews/';
 import BootcampResourcePost from './BootcampResourcePost';
+import CompanyProfile from "../Company/views/CompanyProfile";
 
 class BootcampDashboard extends Component {
   render() {
     return (
       <Fragment>
+        <CompanyProfile />
         <BootcampResourcePost />
         <HackerNews />
         <TechNews />
