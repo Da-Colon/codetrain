4f38170afa78e5101490adcb060d41f6ff23c449
Prescott Sun <sunprescott@gmail.com>
2019-12-12 23:45:16 -0500

    adds component to view user profiles, model, route

--- client/src/component/Dashboard/Dashboards/Bootcamp/BootcampDashboard.jsx
+++ client/src/component/Dashboard/Dashboards/Bootcamp/BootcampDashboard.jsx
@@ -3,11 +3,13 @@ import HackerNews from '../../../NewsFeed/HackerNews/'
 import TechNews from '../../../NewsFeed/TechNews/';
 import BootcampResourcePost from './BootcampResourcePost';
 import CompanyProfile from "../Company/views/CompanyProfile";
+import UserProfile from "./views/BootcampProfile";
 
 class BootcampDashboard extends Component {
   render() {
     return (
       <Fragment>
+        <UserProfile />
         <CompanyProfile />
         <BootcampResourcePost />
         <HackerNews />
