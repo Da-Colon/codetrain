52bc19f0240c562b3a197ed2252220d51004afc3
Nep T. Orshiso <nep.orshiso@gmail.com>
2019-12-14 14:36:42 -0500

    adding logic to render correct applicant component based on user type

--- client/src/component/Applications/index.js
+++ client/src/component/Applications/index.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import { useSelector } from "react-redux";
 
-export default function Applications() {
+import BootcampApplications from "./BootcampApplications";
+import CompanyApplications from "./CompanyApplications";
+
+export default function Home() {
+  const user = useSelector(state => state.user);
+
   return (
     <div>
-      
+      {user.user_types_id === 2 && user.auth === true ? (
+        <BootcampApplications />
+      ) : user.user_types_id === 3 && user.auth === true ? (
+        <CompanyApplications />
+      ) : (
+        window.location.replace("/")
+      )}
     </div>
-  )
+  );
 }
