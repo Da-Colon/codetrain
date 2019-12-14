3ac14d84e6a31388e9cffa897dabe7f9cdf5d6fc
Nep T. Orshiso <nep.orshiso@gmail.com>
2019-12-14 15:45:07 -0500

    drop down form that displays the jobs company user has posted. state updates depending on the job that is currently selected. now need to take the job that is currently selecte dand display applicants for that job post

--- client/src/component/Applications/CompanyApplications.jsx
+++ client/src/component/Applications/CompanyApplications.jsx
@@ -1,12 +1,48 @@
-import React, { Component} from 'react';
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
+import { useSelector } from "react-redux";
 
-class CompanyApplications extends Component {
+const CompanyApplications = () => {
+  const user = useSelector(state => state.user);
+  const [jobs, setJobs] = useState([]);
+  const [jobTitle, setJobTitle] = useState();
 
-  render() { 
-    return ( 
-      <h1>This is the CompanyAPPLICATIONS component</h1>
-    );
+  const getJobPostsByCompanyId = async () => {
+    const endpoint = `http://localhost:3000/posts/jobs/company/${user.companies_id}`;
+    const res = await Axios.get(endpoint);
+    setJobs(res.data);
+    res.data.length > 1 ? setJobTitle([res.data[0].title]) : setJobTitle('')
+  };
+
+  function handleChange(e) {
+    setJobTitle(e.target.value);
   }
-}
- 
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log('jobs is', jobs);
+    console.log('jobId is', jobTitle);
+  }
+
+  useEffect(() => {
+    getJobPostsByCompanyId();
+  }, []);
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <h2>Select a job to display applicants</h2>
+        <select onChange={handleChange}>
+          {jobs.map(job => {
+            return <option value={job.title}>{job.title}</option>;
+          })}
+        </select>
+        <button type="submit">Submit</button>
+      </form>
+
+      <h1>You are currently viewing job title: {jobTitle}</h1>
+    </>
+  );
+};
+
 export default CompanyApplications;
