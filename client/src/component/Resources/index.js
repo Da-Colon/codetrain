dc8237b8bb1d9b0188e9a058b2a7d54535915dbb
Daniel Farlow <dan.farlow@gmail.com>
2019-12-15 10:56:34 -0500

    add ability to get all resources and display them with desired information

--- client/src/component/Resources/index.js
+++ client/src/component/Resources/index.js
@@ -1,15 +1,20 @@
 import React, { Component, Fragment } from 'react';
+import BootcampResourcePost from './BootcampResourcePost';
+import BootcampResourceGet from './BootcampResourceGet';
 
 class Resources extends Component {
   state = {
-    
+
   }
 
-  render() { 
-    return ( 
-      <h1>This is the resources component!</h1>
+  render() {
+    return (
+      <Fragment>
+        <BootcampResourceGet />
+        <BootcampResourcePost />
+      </Fragment>
     );
   }
 }
- 
+
 export default Resources;
