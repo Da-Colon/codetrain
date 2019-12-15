4e3ab2fe67973be144235fec4880fd514475df63
Prescott Sun <sunprescott@gmail.com>
2019-12-15 14:40:05 -0500

    adds landing page

--- client/src/component/Styles/CardContainers.jsx
+++ client/src/component/Styles/CardContainers.jsx
@@ -1,20 +1,28 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 export const MainContainer = styled.div`
   display: flex;
-`
+`;
 
 export const SmallCard = styled.div`
   border: 2px solid black;
   margin: 1rem;
   padding: 8px;
-`
+`;
 
 export const MappedList = styled.ul`
-display: flex;
-flex-direction: column;
+  display: flex;
+  flex-direction: column;
   margin: 16px;
-`
+`;
 export const MappedItem = styled.li`
   margin: 4px;
-`
+`;
+
+export const CenteredContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin: 10vh 20vw;
+`;
