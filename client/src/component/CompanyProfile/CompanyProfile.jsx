6d3f24d4cb8cda930aa6d41f270cbe3ba77d7981
Da-Colon <dcolonantonio@gmail.com>
2019-12-16 12:40:35 -0500

    added report button

--- client/src/component/CompanyProfile/CompanyProfile.jsx
+++ client/src/component/CompanyProfile/CompanyProfile.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import axios from "axios";
-import { useParams } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
 
 const CompanyProfile = () => {
   const company = useSelector(state => state.user);
@@ -24,13 +24,20 @@ const CompanyProfile = () => {
     setCompanyInfo(response.data);
   };
 
+  const history = useHistory()
+  const postReport = () => {
+    history.push(`/report/company/${companyInfo.id}`)
+  }
+
   useEffect(() => {
     getCompanyInfo(id);
   }, []);
 
   return (
     <card>
-      <h1>Company Name: {companyInfo.name}</h1>
+      <h1>Company Name: {companyInfo.name}
+      <button onClick={postReport}>Report {companyInfo.name}</button>
+      </h1>
       <img src={companyInfo.company_logo_url} alt="company logo" />
       <h2>
         Email:{" "}
