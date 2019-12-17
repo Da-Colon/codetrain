27af174e1b7a6e824848c68760cf583aad62de0c
Daniel Farlow <dan.farlow@gmail.com>
2019-12-17 15:04:19 -0500

    abstract away the bootcamp resource card

--- client/src/component/Resources/ResourcePost.jsx
+++ client/src/component/Resources/ResourcePost.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useState, useEffect } from "react";
+import BootcampResourceCard from './BootcampResourceCard';
 import styled from "styled-components";
 import axios from "axios";
 import { useSelector } from "react-redux";
@@ -64,110 +65,30 @@ const ResourcePost = props => {
   return (
     <ResourceWrapper>
       {resourcesFetched ? (
-        // resources.map((resource, i) => {
-        //   const {
-        //     id: resourceId,
-        //     up_votes,
-        //     down_votes,
-        //     title,
-        //     short_description: descriptionShort,
-        //     full_description: descriptionFull,
-        //     resource_url: resourceURL,
-        //     date_posted: datePosted /* information beyond this line relates to resource poster */,
-        //     users_id: usersId,
-        //     email,
-        //     first_name: firstName,
-        //     last_name: lastName,
-        //     github_url: githubLink,
-        //     linkedin_url: linkedinLink,
-        //     bootcamp_name: bootcampAffiliation
-        //   } = resource;
-        //   return (
-        <Card style={{ maxWidth: "60vw", margin: "20px" }}>
-          <CardHeader>
-            <CardHeaderTitle>{resources.title}</CardHeaderTitle>
-            <Link to={`/report/resource/${resources.id}/${resources.users_id}`}>
-              Report Resource
-            </Link>
-          </CardHeader>
-          <CardContent>
-            <Media>
-              <MediaContent hasTextAlign={"left"}>
-                <Title isSize={5}>Resource Info</Title>
-                <Subtitle isSize={6}>
-                  <Anchor href={resources.resource_url}>
-                    Link to resource
-                  </Anchor>
-                  <p>
-                    Posted:{" "}
-                    <Moment format="YYYY-MM-DD">{resources.date_posted}</Moment>
-                  </p>
-                </Subtitle>
-              </MediaContent>
-              <MediaContent hasTextAlign={"right"}>
-                <Title isSize={5}>Resource Creator</Title>
-                <Subtitle isSize={6}>
-                  <Breadcrumb isSize={`small`} isAlign={"right"}>
-                    <ul>
-                      <BreadcrumbItem>
-                        <Link to={`/user/${resources.users_id}`}>
-                          {resources.first_name} {resources.last_name}
-                        </Link>
-                      </BreadcrumbItem>
-                      <BreadcrumbItem>
-                        <a href={`mailto:${resources.email}`}>Email</a>
-                      </BreadcrumbItem>
-                    </ul>
-                    <ul>
-                      <BreadcrumbItem>
-                        <a href={resources.github_url}>GitHub</a>
-                      </BreadcrumbItem>
-                      <BreadcrumbItem>
-                        <a href={resources.linkin_url}>LinkedIn</a>
-                      </BreadcrumbItem>
-                    </ul>
-                  </Breadcrumb>
-                </Subtitle>
-              </MediaContent>
-            </Media>
-            <Content>
-              <Box>{resources.short_description}</Box>
-            </Content>
-            <Content>{resources.full_description}</Content>
-          </CardContent>
-          {user.id === resources.users_id ? (
-            <CardFooter>
-              <CardFooterItem>
-                <Button
-                  isColor={`success`}
-                  onClick={e => editResource(e, resources)}
-                >
-                  Edit
-                </Button>
-              </CardFooterItem>
-              <CardFooterItem>
-                <Button
-                  isColor={`danger`}
-                  onClick={() => deleteResource(resources.id)}
-                >
-                  Delete
-                </Button>
-              </CardFooterItem>
-              {editFormActive.resourceId === resources.id ? (
-                <EditResourceModal
-                  editFormActive={editFormActive}
-                  setEditFormActive={setEditFormActive}
-                  resource={resources}
-                />
-              ) : null}
-            </CardFooter>
-          ) : null}
-        </Card>
+        <BootcampResourceCard
+          title={resources.title}
+          resourceURL={resources.resource_url}
+          cardResource={resources}
+          resourceId={resources.id}
+          datePosted={resources.date_posted}
+          usersId={resources.users_id}
+          firstName={resources.first_name}
+          lastName={resources.last_name}
+          email={resources.email}
+          githubLink={resources.github_url}
+          linkedinLink={resources.linkin_url}
+          descriptionShort={resources.short_description}
+          descriptionFull={resources.full_description}
+          user={user}
+          editResource={editResource}
+          deleteResource={deleteResource}
+          editFormActive={editFormActive}
+          setEditFormActive={setEditFormActive}
+          fetchResourcesData={fetchResourcesData}
+        />
       ) : (
-        //   );
-        // })
-        <p>Loading ...</p>
-      )}
+          <p>Loading ...</p>
+        )}
     </ResourceWrapper>
   );
 };
