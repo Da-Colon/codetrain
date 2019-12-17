3515333940b02ec5f4a13b039402741254598766
Da-Colon <dcolonantonio@gmail.com>
2019-12-17 02:01:10 -0500

    added bloomer  panel started layout

--- client/src/component/Home/admin/reports.jsx
+++ client/src/component/Home/admin/reports.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import {useSelector} from 'react-redux'
+import {Panel, PanelBlock, PanelHeading, PanelIcon, PanelTab, PanelTabs, Control, Input, Icon} from 'bloomer'
 
-export default function reports() {
+//   getAllCompanyReports,
+//   getAllJobReports,
+//   getAllResourceReports,
+//   getAllUserReports,
+//   sendMessageCompany,
+//   sendMessageUser,
+//   postReport,
+//   deleteJob,
+//   deleteResource,
+//   removeAuthCompanyUsers,
+//   removeAuthUser,
+//   getReports,
+//   resolveIssue
+
+export default function Reports() {
+  const user = useSelector(state => state.user);
+
+  if (user.user_types_id !== 1) {
+    window.location.replace("/");
+  }
+
+  const [users, setUsers] = useState([]);
+  const [companies, setCompanies] = useState([]);
+  const [jobs, setJobs] = useState([]);
+  const [resources, setResources] = useState([]);
+
   return (
     <div>
-      
+      <Panel style={{width: "250px", height: "88vh", overflowY: "scroll", overflowX: "hidden"}}>
+    <PanelHeading>Reports</PanelHeading>
+    {/* <PanelBlock>
+        <Control hasIcons='left'>
+            <Input isSize='small' placeholder='Search' />
+            <Icon isSize='small' isAlign='left'>
+                <span className='fa fa-search' aria-hidden='true' />
+            </Icon>
+        </Control>
+    </PanelBlock> */}
+    <PanelTabs>
+        <PanelTab isActive>Unresolved</PanelTab>
+        <PanelTab>Resolved</PanelTab>
+
+    </PanelTabs>
+    <PanelBlock isActive>
+        <PanelIcon className="fa fa-code-fork" />
+        RxJS
+    </PanelBlock>
+</Panel>
     </div>
   )
 }
