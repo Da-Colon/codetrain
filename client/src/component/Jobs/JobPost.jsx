3e1917e8d40d5b7db053c33a54055df385e4bb02
Nep T. Orshiso <nep.orshiso@gmail.com>
2019-12-16 23:18:48 -0500

    Ready to send payload from client. need to hook up to express

--- client/src/component/Jobs/JobPost.jsx
+++ client/src/component/Jobs/JobPost.jsx
@@ -14,13 +14,28 @@ import {
   CardFooterItem
 } from "bloomer";
 import styled from "styled-components";
-
-import { Button } from "../Styles/FormStyles";
+import {
+  Form,
+  Label,
+  Input,
+  Button,
+  Title,
+  TextArea
+} from "../Styles/FormStyles";
 
 const JobPost = props => {
   const user = useSelector(state => state.user);
-  console.log("user data is", user);
+  const [isEditMode, setEditMode] = useState(false);
   const [jobs, setJobs] = useState([]);
+
+  const [editJobState, setEditJobState] = useState({
+    job_title: "",
+    description: "",
+    experience: "",
+    email: "",
+    phone: "",
+    isSubmitted: false
+  });
 
   const fetchJobsData = async () => {
     const endpoint = `http://localhost:3000/posts/jobs/id/${props.match.params.job_id}`;
@@ -44,47 +59,143 @@ const JobPost = props => {
       : alert("Sorry. There was an error.");
   };
 
+  const handleEditMode = () => {
+    setEditJobState({
+      job_title: jobs.title,
+      description: jobs.description,
+      experience: jobs.experience,
+      email: jobs.email,
+      phone: jobs.phone
+    });
+    setEditMode(true);
+  };
+
+  // prevents the use of writing several handleChange functions by deconstructing name and value from the onchange event
+  const handleChange = e => {
+    const { name, value } = e.target;
+    setEditJobState({ ...editJobState, [name]: value });
+  };
+
+  const handleEditSubmit = async e => {
+    e.preventDefault();
+    const endpoint = `http://localhost:3000/posts/jobs/update/${jobs.id}`;
+
+    const payload = {
+      title: editJobState.job_title,
+      content: editJobState.description,
+      experience: editJobState.experience,
+      contact_email: editJobState.email,
+      contact_phone: editJobState.phone,
+      companies_id: jobs.companies_id,
+      users_id: user.companies_id
+    };
+
+    console.log(payload)
+
+    const response = await Axios.put(endpoint, payload);
+    setEditMode(false)
+    // alert("Your job was posted!");
+    // setState({ ...state, isSubmitted: true });
+    // history.push("/jobs");
+  };
+
   return (
-    <JobCardWrapper>
-      <Card style={{ maxWidth: "60vw", margin: "20px" }}>
-        <CardHeader>
-          <CardHeaderTitle>{jobs.title}</CardHeaderTitle>
-        </CardHeader>
-        <CardContent>
-          <Content>
-            <strong>Date Posted: </strong>
-            <Moment format="YYYY-MM-DD">{jobs.date_posted}</Moment>
-          </Content>
-          <Content>
-            <strong>Job Description: </strong>
-            {jobs.content}
-          </Content>
-          <Content>
-            <strong>Experience:</strong> {jobs.experience}
-          </Content>
-          <Content>
-            <strong>Company Name: </strong>
-            <Link to={`/company/${jobs.companies_id}`}>{jobs.name}</Link>
-          </Content>
-          <Content>
-            <strong>Contact Email: </strong>
-            {jobs.contact_email}
-          </Content>
-        </CardContent>
-        <CardFooter>
-          <CardFooterItem>
-            {/* If it's a bootcamp user viewing the job, give them ability to apply. If it's a user representing the company that posted the job, give them the option to apply. */}
-            {user.id === 2 ? (
-              <Button onClick={postApplication}>Apply!</Button>
-            ) : user.companies_id === jobs.companies_id ? (
-              <Button>Edit Post</Button>
-            ) : (
-              <></>
-            )}
-          </CardFooterItem>
-        </CardFooter>
-      </Card>
-    </JobCardWrapper>
+    <>
+      {isEditMode ? (
+        <Form onSubmit={handleEditSubmit}>
+          <Label>
+            Job Title
+            <Input
+              type="text"
+              placeholder="Job Title"
+              name="job_title"
+              value={editJobState.job_title}
+              onChange={handleChange}
+            ></Input>
+          </Label>
+          <Label>
+            Job Description
+            <TextArea
+              placeholder="Job Description Information"
+              name="description"
+              value={editJobState.description}
+              onChange={handleChange}
+            ></TextArea>
+          </Label>
+          <Label>
+            Experience Desired
+            <TextArea
+              placeholder="What skills are you looking for?"
+              name="experience"
+              value={editJobState.experience}
+              onChange={handleChange}
+            ></TextArea>
+          </Label>
+          <Label>
+            Contact Email
+            <Input
+              type="email"
+              placeholder="Contact Email"
+              name="email"
+              value={editJobState.email}
+              onChange={handleChange}
+            ></Input>
+          </Label>
+          <Label>
+            Contact Phone Number
+            <Input
+              type="tel"
+              placeholder="Phone Number"
+              name="phone"
+              value={editJobState.phone}
+              onChange={handleChange}
+            ></Input>
+          </Label>
+          <Button type="submit">Edit Job Post</Button>
+        </Form>
+      ) : (
+        <CardFooterItem>
+          <Card style={{ maxWidth: "60vw", margin: "20px" }}>
+            <CardHeader>
+              <CardHeaderTitle>{jobs.title}</CardHeaderTitle>
+            </CardHeader>
+            <CardContent>
+              <Content>
+                <strong>Date Posted: </strong>
+                <Moment format="YYYY-MM-DD">{jobs.date_posted}</Moment>
+              </Content>
+              <Content>
+                <strong>Job Description: </strong>
+                {jobs.content}
+              </Content>
+              <Content>
+                <strong>Experience:</strong> {jobs.experience}
+              </Content>
+              <Content>
+                <strong>Company Name: </strong>
+                <Link to={`/company/${jobs.companies_id}`}>{jobs.name}</Link>
+              </Content>
+              <Content>
+                <strong>Contact Email: </strong>
+                {jobs.contact_email}
+              </Content>
+            </CardContent>
+            <CardFooter>
+              <CardFooterItem>
+                {/* If it's a bootcamp user viewing the job, give them ability to apply. If it's a user representing the company that posted the job, give them the option to apply. */}
+                {user.id === 2 ? (
+                  <Button onClick={postApplication}>Apply!</Button>
+                ) : user.companies_id === jobs.companies_id ? (
+                  <Button onClick={handleEditMode}>Edit Post</Button>
+                ) : (
+                  <></>
+                )}
+              </CardFooterItem>
+            </CardFooter>
+          </Card>
+        </CardFooterItem>
+      )}
+    </>
   );
 };
 
