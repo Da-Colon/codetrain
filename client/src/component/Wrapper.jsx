d6c3949304a20434f2a6c1e104daead5795a09f2
Prescott Sun <sunprescott@gmail.com>
2019-12-18 16:39:19 -0500

    Adds container styling to landing, profiles, and application views

---
+++ client/src/component/Wrapper.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  // min-height: calc(100vh - 64px);
+  // margin: 0 auto;
+  // max-width: 90vx;
+`;
+
+export default Wrapper;
