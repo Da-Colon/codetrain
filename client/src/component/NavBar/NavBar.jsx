790b68dd45fb4da560788def40e20cdd8f741ea8
Nep T. Orshiso <nep.orshiso@gmail.com>
2019-12-18 13:06:31 -0500

    beginning navbar bloomer work

--- client/src/component/NavBar/NavBar.jsx
+++ client/src/component/NavBar/NavBar.jsx
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import { theStore } from "../../index";
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarItem,
+  Icon,
+  NavbarBurger,
+  NavbarMenu,
+  NavbarStart,
+  NavbarLink,
+  NavbarDropdown,
+  NavbarDivider,
+  NavbarEnd,
+  Field,
+  Control,
+  Button
+} from "bloomer";
 
-import { Link, Nav, Ul, Button } from "../Styles/navStyles";
+import ColorTheme from '../Styles/Colors'
 
-const NavBar = () => {
+const NavBarBloomer = () => {
   const user = useSelector(state => state.user);
 
   const handleLogout = async e => {
@@ -15,76 +31,71 @@ const NavBar = () => {
     window.location.replace("/");
   };
 
-  return (
-    <Nav>
-      <img src={user.profile_pic_url} />
-      <Ul>
-        <Link
-          exact
-          to="/"
-          className="brand-logo"
-          style={{ backgroundColor: "unset" }}
-        >
-          codetrain
-        </Link>
-        <Link to="/resources">Resources</Link>
-        <h1>
-          {user.first_name} {user.last_name}
-        </h1>
-      </Ul>
+  const onClickNav = () => setIsActive(!isActive);
+  const [isActive, setIsActive] = useState(false);
+  const { background } = ColorTheme.elements;
 
-      {user.user_types_id === 1 && user.auth === true ? (
-        //  Admin User NavBar
-        <Ul>
-          <Link to="/home">Home</Link>
-          <Link to="/admin/reports">Reports</Link>
-          <Link to="/jobs" exact>
-            Jobs
-          </Link>
-          <Link to="/messages">Messages</Link>
-          <Button onClick={handleLogout}>Logout</Button>
-        </Ul>
-      ) : user.user_types_id === 2 && user.auth === true ? (
-        //  BootCamp User NavBar
-        <Ul>
-          <Link to="/home">Home</Link>
-          <Link to={`/user/${user.id}`}>Profile</Link>
-          <Link to="/jobs" exact>
-            Jobs
-          </Link>
-          <Link to="/applications">Applications</Link>
-          <Link to="/messages">Messages</Link>
-          <Button onClick={handleLogout}>Logout</Button>
-        </Ul>
-      ) : user.user_types_id === 3 && user.auth === true ? (
-        // Company User NavBar
-        <Ul>
-          <Link to="/home">Home</Link>
-          <Link to={`/company/${user.companies_id}`}>Company</Link>
-          <Link to="/jobs">Jobs</Link>
-          <Link to="/applications">Applicants</Link>
-          <Link to="/messages">Messages</Link>
-          <Button onClick={handleLogout}>Logout</Button>
-        </Ul>
-      ) : (user.user_types_id === 2 || user.user_types_id === 3) &&
-        user.auth === false ? (
-        // Not Auth User
-        <Ul>
-          <Link to="/profile">Profile</Link>
-          <Button onClick={handleLogout}>Logout</Button>
-        </Ul>
-      ) : (
-        <Ul>
-          <li>
-            <Link to="/signup">Sign up</Link>
-          </li>
-          <li>
-            <Link to="/login">Log in</Link>
-          </li>
-        </Ul>
-      )}
-    </Nav>
+  return (
+    <Navbar style={{ borderBottom: "solid 1px black", margin: "0", backgroundColor: `${background}`}}>
+      <NavbarBrand>
+        <NavbarItem>
+        </NavbarItem>
+        <NavbarItem isHidden="desktop">
+          <Icon className="fa fa-github" />
+        </NavbarItem>
+        <NavbarItem isHidden="desktop">
+          <Icon className="fa fa-twitter" style={{ color: "#55acee" }} />
+        </NavbarItem>
+        <NavbarBurger
+          isActive={isActive}
+          onClick={onClickNav}
+        />
+      </NavbarBrand>
+      <NavbarMenu isActive={isActive} onClick={onClickNav}>
+        <NavbarStart>
+          <NavbarItem href="#/">Home</NavbarItem>
+          <NavbarItem hasDropdown isHoverable>
+            <NavbarLink href="#/documentation">Documentation</NavbarLink>
+            <NavbarDropdown>
+              <NavbarItem href="#/">One A</NavbarItem>
+              <NavbarItem href="#/">Two B</NavbarItem>
+              <NavbarDivider />
+              <NavbarItem href="#/">Two A</NavbarItem>
+            </NavbarDropdown>
+          </NavbarItem>
+        </NavbarStart>
+        <NavbarEnd>
+          <NavbarItem
+            href="https://github.com/AlgusDark/bloomer"
+            isHidden="touch"
+          >
+            <Icon className="fa fa-github" />
+          </NavbarItem>
+          <NavbarItem href="https://twitter.com/AlgusDark" isHidden="touch">
+            <Icon className="fa fa-twitter" style={{ color: "#55acee" }} />
+          </NavbarItem>
+          <NavbarItem>
+            <Field isGrouped>
+              <Control>
+                <Button
+                  id="twitter"
+                  data-social-network="Twitter"
+                  data-social-action="tweet"
+                  data-social-target="http://bloomer.js.org"
+                  target="_blank"
+                  href="https://twitter.com/intent/tweet?text=bloomer:
+                    a set of React Stateless Components for bulma.io&amp;url=http://bloomer.js.org&amp;via=AlgusDark"
+                >
+                  <Icon className="fa fa-twitter" />
+                  <span>Tweet</span>
+                </Button>
+              </Control>
+            </Field>
+          </NavbarItem>
+        </NavbarEnd>
+      </NavbarMenu>
+    </Navbar>
   );
 };
 
-export default NavBar;
+export default NavBarBloomer;
