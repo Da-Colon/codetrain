50da5edfc1ed435d49742f3bb2bad92333196246
Nep T. Orshiso <nep.orshiso@gmail.com>
2019-12-18 23:28:55 -0500

    a few entries for job seed data

---
+++ server/SQL/nep.sql
@@ -0,0 +1,92 @@
+INSERT INTO posts_jobs (
+    title,
+    content,
+    experience,
+    contact_email,
+    contact_phone,
+    companies_id,
+    users_id
+  )
+VALUES
+  (
+    '.Net core and .Net core 2 one CRUD page',
+    'We need to build one page to manage CRUD for one Entity
+
+The main components are:
+
+- Grid of records: where user can filter/sort and delete records
+
+- Add new record
+
+- Edit exist record
+
+The entity has the following fields (11 fields):
+
+- ID: text, max length 25 characters
+
+- Name: text, max length 50 characters
+
+- Description: text, max length 255 characters
+
+- Pillar: Drop-down (few options, read from Pillars table)
+
+- Category: Drop-down (many options, read from Categories table, and filters based on selected Pillar)
+
+- Sub Category: Drop-down (many options, read from Sub_Categories table, and filters based on selected Category)
+
+- Unit: Drop-down (few options, read from Units table)
+
+- Source: Drop-down (few options, read from Sources table)
+
+- Form: Drop-down (few options, read from Sources table)
+
+- Expression: text, max length 255 characters
+
+- YTD Expression: text, max length 255 characters',
+    'Technologies:
+
+- .Net core 2+ (C#)
+
+- Angular 6+
+
+- Bootstrap 4',
+    'hmnep@gmail.com',
+    '1-867-5309',
+    1,
+    6
+  ),
+  (
+    'For 2020 All-round awesome Front-end developer for start-up',
+    'We are looking for an all-round awesome Front End developer who is creative, has a good eye for UI/UX, works fast, is detailed oriented and can meet deadlines.
+
+You need a strong command of HTML/CSS/JS/Bootstrap.
+
+They have a solid understanding of React/Angular/VueJS.
+
+You must have experience building responsive websites ([login to view URL] compatible).
+
+If you are passionate about sustainability and social impact, then this will be a great project for you.',
+    'We are looking for someone who can work with CSS, HTML5, Javascript, and Angular.js',
+    'hmnep@gmail.com',
+    '1-867-5309',
+    1,
+    6
+  ),
+  (
+    'Integrate MailChimp with Web Application',
+    'We are an HR Tech startup. We need to integrate our core application with MailChimp using standard MailChimp APIs. Looking for a developer who has prior experience with MailChimp APIs and can start work immediately. Our application is an AWS native application built using MEAN stack. We are based out of Bangalore.',
+    'Mailchimp, AWS Lambda, MongoDB, Amazon App Development, Javascript',
+    'hmnep@gmail.com',
+    '1-867-5309',
+    1,
+    6
+  ),
+  (
+    'Anngular Developer',
+    'As developer for the Web, he/she must know about HTTP (and siblings), HTML, CSS, JavaScript, JQuery, Ajax, JSON, multiple browsers (and their individual differences), tools, and programs that deal with Web communications. You must know about the human cognition, basic psychology, and logic sorting. You should also have good common sense and must know about all technologies offerings in accessibility and usability, interaction principles, MVC (Model-View-Controller pattern) separation, languages for creating interfaces, and generally tool kits, tools and programs that deal with the interface.',
+    'HTTP, HTM, CSS, JavaScript, JQuery, Ajax, JSON',
+    'hmnep@gmail.com',
+    '1-867-5309',
+    1,
+    6
+  )
